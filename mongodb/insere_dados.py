f7666c79183963b9f8e93c5f00d4abe8338625f9
thiagopermuniz@gmail.com <thiagopermuniz@gmail.com>
2023-04-29 19:20:30 -0300

    remoção codgoi sujo

--- mongodb/insere_dados.py
+++ mongodb/insere_dados.py
@@ -5,24 +5,24 @@ from pymongo import MongoClient
 
 
 # busca dados da API IBGE
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes"
+url = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
 response = requests.get(url)
 json_list = response.json()
 
 # conecta na base
 client = MongoClient('mongodb://localhost:27017/')
-db = client.mydb
-collection = db.mycollection
+db = client.db
+collection = db.distritos
 
 
 # insere dados
 try:
     start_time = time.time()
     for json_obj in json_list:
-        json_str = json.dumps(json_obj)
-        collection.insert_one(json_str)
+        json_dict = json.loads(json.dumps(json_obj))
+        collection.insert_one(json_dict)
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"tempo insert: {elapsed_time} secs")
+    print(f"{len(json_list)} registros: tempo insert: {elapsed_time} secs")
 except Exception as err:
     print(err)
